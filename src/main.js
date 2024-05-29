b55a33677d1179a0ad5be01b74eef4d0f3f224b3
Ivan Nelson <ivan.sryvkin@bk.ru>
2024-05-29 13:36:48 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router/router";
-import "./style.css";
-import Vue3Toastify from "vue3-toastify";
-import 'vue3-toastify/dist/index.css';
-import store from "./store/store";
+// Импортируем необходимые модули и стили
+import { createApp } from "vue"; // Функция для создания приложения Vue
+import App from "./App.vue"; // Основной компонент приложения
+import router from "./router/router"; // Маршрутизатор приложения
+import "./style.css"; // Общие стили приложения
+import Vue3Toastify from "vue3-toastify"; // Пакет для уведомлений
+import 'vue3-toastify/dist/index.css'; // Стили для уведомлений
+import store from "./store/store"; // Хранилище состояния Vuex
+
+// Создаем приложение Vue
 createApp(App)
-  .use(router)
-	.use(store)
+  .use(router) // Подключаем маршрутизатор
+  .use(store) // Подключаем хранилище состояния Vuex
   .use(Vue3Toastify, {
-    autoClose: 3000,
+    autoClose: 3000, // Настраиваем автоматическое закрытие уведомлений через 3000 миллисекунд
   })
-  .mount("#app");
+  .mount("#app"); // Монтируем приложение на элемент с id "app"
